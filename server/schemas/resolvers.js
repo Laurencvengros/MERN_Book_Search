b1a6da55705105e4c1eb564a1035c13beff95c97
Lauren <l.cvengros@icloud.com>
2022-12-12 15:42:35 -0500

    added mutation to resolvers.js to add a user and assign them a token and log them in (or throw an error if incorrect credentials

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,8 +13,33 @@ const resolvers = {
                 return userData
             }
             throw new AuthenticationError('You need to log in first!');
-        }
-    }
+        },
+    },
+    Mutation: {
+        addUser: async (parent, {name, email, password}) => {
+            const user = await User.create({name, email, password});
+            const token = signToken(user);
+
+            return {token, profile};
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if(!user){
+                throw new AuthenticationError('Incorrect email or password');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw){
+                throw new AuthenticationError('Incorrect email or password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+    },
+
 };
 
 module.exports = resolvers;
