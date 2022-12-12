74d15c8c7d61ab177e185335841d0e7de324c439
Lauren <l.cvengros@icloud.com>
2022-12-12 17:16:38 -0500

    added code in app.js for login token and graphql link

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,48 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
+const authLink = setContext((_, { headers }) =>{
+  const token = localStorage.getItem('id_token');
 
+  return{
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+  );
+}
 
 export default App;
