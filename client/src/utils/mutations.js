63d330c024a53f1efb887b2a51f00f192daaec52
Lauren <l.cvengros@icloud.com>
2022-12-12 17:04:54 -0500

    added REMOVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,11 +13,12 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!){
-        addUser(usernem: $username, email: $email, password: $password){
+        addUser(username: $username, email: $email, password: $password){
             token
             user{
                 _id
                 username
+                email
             }
         }
     }
@@ -39,6 +40,25 @@ export const SAVE_BOOK = gql`
                 image
                 link
             
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                authors
+                description
+                title
+                image
+                link
             }
         }
     }
