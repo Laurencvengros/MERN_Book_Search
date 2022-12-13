71670b7deb30784f25d82b64e766957442c9c5ef
Lauren <l.cvengros@icloud.com>
2022-12-13 10:26:40 -0500

    fixed error in resolvers to match delete function in typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -48,7 +48,7 @@ const resolvers = {
             }
             throw new AuthenticationError('Please login first');
         },
-        deleteBook: async (parent, { bookId }, context) =>{
+        removeBook: async (parent, { bookId }, context) =>{
             if(context.user){
                 const updatedUser = await User.findOneAndUpdate(
                     {_id: context.user._id},
