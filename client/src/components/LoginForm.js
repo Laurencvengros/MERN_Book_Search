bdbbc017b8444e9f2a95c44bdce459260d142311
Lauren <l.cvengros@icloud.com>
2022-12-13 10:18:42 -0500

    updated handleFormSubmit function in LoginForm.js to accept the login variable from LOGIN_USER

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -13,6 +13,8 @@ const LoginForm = () => {
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  const [loginUser] =useMutation(LOGIN_USER)
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -29,15 +31,10 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({variables: { ...userFormData }});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token)
     } catch (err) {
       console.error(err);
       setShowAlert(true);
